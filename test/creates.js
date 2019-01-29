f4ef47b45f661d2300676fe773fe4cfdc5a4a445
Eeshan Garg <jerryguitarist@gmail.com>
2019-01-29 01:06:44 -0330

    creates: Add a Zapier Create for sending a stream message.

---
+++ test/creates.js
@@ -0,0 +1,43 @@
+/* global it */
+/* global describe */
+
+require('should');
+
+const zapier = require('zapier-platform-core');
+
+const App = require('../index');
+const appTester = zapier.createAppTester(App);
+
+const nock = require('nock');
+
+describe('creates', () => {
+
+    it('should send a stream message', (done) => {
+
+        const bundle = {
+            authData: {
+                api_key: 'secret',
+                subdomain: 'yourzulipsubdomain',
+            },
+            inputData: {
+                stream: 'test',
+                topic: 'Sample topic',
+                content: 'Sample content'
+            }
+        };
+
+        // mocks the next request that matches this url and querystring
+        nock('https://yourzulipsubdomain.zulipchat.com')
+            .post('/api/v1/external/zapier?stream=test&api_key=secret')
+            .reply(200, { result: 'success', msg: '' });
+
+        appTester(App.creates.stream_message.operation.perform, bundle)
+            .then((json_response) => {
+                json_response.should.have.property('msg');
+                json_response.result.should.eql('success');
+                done();
+            })
+            .catch(done);
+
+    });
+});
