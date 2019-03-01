7485431dc5b65f34765a94cab50f9dcfe83e3134
Eeshan Garg <jerryguitarist@gmail.com>
2019-03-01 09:35:26 -0330

    stream_message: Add pre-populated field for recipient streams.

--- test/fields.js
+++ test/fields.js
@@ -65,4 +65,54 @@ describe('custom fields', () => {
             })
             .catch(done);
     });
+    it('should return an object with stream_id:stream_name pairs', (done) => {
+
+        const bundle = {
+            authData: {
+                api_key: 'secret',
+                domain: 'https://yourzulipsubdomain.zulipchat.com/api/v1',
+                username: 'zapierbot@zulip.com'
+            },
+        };
+
+        const payload = {
+            'msg': '',
+            'result': 'success',
+            'streams': [
+                {
+                    'name': 'Denmark',
+                    'rendered_description': '<p>A Scandinavian country</p>',
+                    'stream_id': 1,
+                    'is_announcement_only': false,
+                    'invite_only': false,
+                    'description': 'A Scandinavian country',
+                    'history_public_to_subscribers': true
+                },
+                {
+                    'name': 'Rome',
+                    'rendered_description': '<p>Yet another Italian city</p>',
+                    'stream_id': 2,
+                    'is_announcement_only': false,
+                    'invite_only': false,
+                    'description': 'Yet another Italian city',
+                    'history_public_to_subscribers': true
+                }
+            ]
+        };
+
+        // mocks the next request that matches this url and querystring
+        nock('https://yourzulipsubdomain.zulipchat.com')
+            .get('/api/v1/streams?include_public=true&include_subscribed=true&include_owner_subscribed=true')
+            .reply(200, payload);
+
+        appTester(App.creates.stream_message.operation.inputFields[0], bundle)
+            .then((json_response) => {
+                json_response.choices.should.eql({
+                    1: 'Denmark',
+                    2: 'Rome'
+                });
+                done();
+            })
+            .catch(done);
+    });
 });
